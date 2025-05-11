5571a2a0979e17794d2be7f8a1844204828c0555
Mashopy <eliasgheeraert@gmail.com>
2025-05-11 15:48:41 +0200

    kleaf: Use `KLEAF_SUPPORTED_PROJECTS` instead of `PROJECT`
    BSP doesn't even set PROJECT anywhere, what was even the point to add it ?

--- build.sh
+++ build.sh
@@ -7,10 +7,6 @@ DEVICE_MODULES_DIR=$(basename $(dirname $0))
 source "${DEVICE_MODULES_DIR}/kernel/kleaf/_setup_env.sh"
 
 # run kleaf commands or legacy build.sh
-result=$(echo ${KLEAF_SUPPORTED_PROJECTS} | grep -wo ${PROJECT}) || result=""
-if [[ ${result} != "" ]]
-then # run kleaf commands
-
 build_scope=internal
 if [ ! -d "vendor/mediatek/tests/kernel" ]
 then
@@ -20,11 +16,11 @@ fi
 if [ -z ${TARGET} ]
 then
   TARGET=${build_scope}_modules_install
-  KLEAF_BUILD_TARGET=//${DEVICE_MODULES_DIR}:${PROJECT}_${TARGET}.${MODE}
+  KLEAF_BUILD_TARGET=//${DEVICE_MODULES_DIR}:${KLEAF_SUPPORTED_PROJECTS}_${TARGET}.${MODE}
 else
   KLEAF_BUILD_TARGET=${TARGET}.${MODE}
 fi
-KLEAF_DIST_TARGET=//${DEVICE_MODULES_DIR}:${PROJECT}_${build_scope}_dist.${MODE}
+KLEAF_DIST_TARGET=//${DEVICE_MODULES_DIR}:${KLEAF_SUPPORTED_PROJECTS}_${build_scope}_dist.${MODE}
 KLEAF_OUT=("--output_user_root=${OUT_DIR} --output_base=${OUT_DIR}/bazel/output_user_root/output_base")
 KLEAF_ARGS=("${DEBUG_ARGS} ${SANDBOX_ARGS} \
 	--experimental_writable_outputs --allow_ddk_unsafe_headers=1 --workaround_btrfs_b292212788 \
@@ -40,13 +36,3 @@ set -x
 	--nokmi_symbol_list_violations_check ${KLEAF_DIST_TARGET} -- --dist_dir=${OUT_DIR}/dist
 )
 set +x
-
-else # run legacy build.sh
-set -x
-(
-  OUT_DIR=${OUT_DIR} python ${DEVICE_MODULES_DIR}/scripts/gen_build_config.py -p ${PROJECT} \
-	-o ${BUILD_CONFIG}.legacy -m ${MODE} --kernel-defconfig-overlays "${DEFCONFIG_OVERLAYS}"
-  OUT_DIR=${OUT_DIR} BUILD_CONFIG=${BUILD_CONFIG}.legacy CC_WRAPPER=${CC_WRAPPER} build/build.sh
-)
-set +x
-fi
