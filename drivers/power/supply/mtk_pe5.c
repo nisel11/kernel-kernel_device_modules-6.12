04248d7c8b6110e39b67dcc2cdff936f6023eae0
Li Hongtao <liht17@motorola.com>
2025-09-03 18:59:12 +0800

    charge: config pe5 ibat ocp
    config pe5 ibat ocp
    default max ibat current is 6A
    
    refer to https://gerrit.mot.com/#/c/2795073/
    
    Change-Id: Ib795c8d5971d617f5eb6dd0ca75b92c417ed9bbb
    Signed-off-by: Li Hongtao <liht17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3538467
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Obi-Wan Kenobi
    Tested-by: Jira Key
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mtk_pe5.c
+++ drivers/power/supply/mtk_pe5.c
@@ -807,7 +807,7 @@ static u32 pe50_get_ibatocp(struct pe50_algo_info *info, u32 ita)
 #else
 	struct pe50_algo_data *data = info->data;
 	u32 ibat;
-	ibat = data->mmi_fcc_limit;
+	ibat = data->mmi_max_ibat;
 	return percent(ibat, PE50_IBATOCP_RATIO);
 #endif
 }
@@ -4243,6 +4243,14 @@ static int pe50_parse_dt(struct pe50_algo_info *info)
 	}
 	PE50_INFO("mmi thermal dts= %d,%d,%d\n",
 		data->mmi_therm_cur_thres, data->mmi_therm_vol_thres, data->mmi_therm_step);
+
+	if (of_property_read_u32(np, "mmi_max_ibat", &val) >= 0)
+		data->mmi_max_ibat = val;
+	else {
+		pr_notice("mmi_max_ibat using default:%d\n",
+			MMI_MAX_IBAT);
+		data->mmi_max_ibat = MMI_MAX_IBAT;
+	}
 
 	return 0;
 }
