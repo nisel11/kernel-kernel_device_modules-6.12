43a6bb156017a3bc6857676ecfdd3e40758ab962
Li Hongtao <liht17@motorola.com>
2025-09-04 19:07:58 +0800

    charge: fix fg 100% tracking feature don't work
    Sometimes battery reach real FULL,but fg don't show 100%,and
    100% tracking feature don't work to track to 100%. Because charger
    exist status is false.
    
    refer to https://gerrit.mot.com/#/c/2811626/
    
    Change-Id: I8e7eecf38e5b5f51e332c76b45e65180d4e7624b
    Signed-off-by: Li Hongtao <liht17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3540705
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mtk_battery_daemon.c
+++ drivers/power/supply/mtk_battery_daemon.c
@@ -3427,7 +3427,8 @@ static void mtk_battery_daemon_handler(struct mtk_battery *gm, void *nl_data,
 	{
 		int is_charger_exist = 0;
 
-		if (gm->bm->bs_data.bat_status == POWER_SUPPLY_STATUS_CHARGING)
+		if (gm->bm->bs_data.bat_status == POWER_SUPPLY_STATUS_CHARGING ||
+			gm->bm->bs_data.bat_status == POWER_SUPPLY_STATUS_FULL)
 			is_charger_exist = true;
 		else
 			is_charger_exist = false;
