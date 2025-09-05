3ebe534835022a038d7a82c997d3d294e915e1ba
Li Hongtao <liht17@motorola.com>
2025-09-05 10:03:57 +0800

    LPD: update LPD uevent
    Reboot unit when trigger LPD, upper can't got LPD uevent.
    So update LPD uevent in loop.
    
    refer to https://gerrit.mot.com/#/c/2813411/
    
    Change-Id: I1b07fff59475dc66aeba8dc21adb401dd9030d72
    Signed-off-by: Li Hongtao <liht17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3541673
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mt6375-charger.c
+++ drivers/power/supply/mt6375-charger.c
@@ -268,6 +268,7 @@ struct mt6375_chg_data {
 	int	qc_chg_type;
 
 	struct power_supply *batt_psy;
+	struct power_supply *wlc_psy;
 	char				*batt_uenvp[2];
 };
 
@@ -1012,6 +1013,7 @@ static void mt6375_chg_attach_pre_process(struct mt6375_chg_data *ddata,
 static int mmi_notify_vbus_event(struct mt6375_chg_data *ddata, bool vbus_status)
 {
 	char *event_string = NULL;
+	union power_supply_propval val = {0};
 
 	if(!ddata->batt_psy)
 		ddata->batt_psy = power_supply_get_by_name("battery");
@@ -1019,6 +1021,16 @@ static int mmi_notify_vbus_event(struct mt6375_chg_data *ddata, bool vbus_status
 		dev_notice(ddata->dev,
 			"%s: get battery supply failed\n", __func__);
 		return -EINVAL;
+	}
+
+	if(!ddata->wlc_psy)
+		ddata->wlc_psy = power_supply_get_by_name("wireless");
+
+	if (ddata->wlc_psy) {
+		power_supply_get_property(ddata->wlc_psy,
+				POWER_SUPPLY_PROP_ONLINE, &val);
+		if (val.intval)
+			return 0;
 	}
 
 	event_string = kmalloc(CHG_SHOW_MAX_SIEZE, GFP_KERNEL);
