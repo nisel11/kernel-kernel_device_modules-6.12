88530fe50f2e256464b35d53f7286818e66f8d38
Li Hongtao <liht17@motorola.com>
2025-09-09 20:11:53 +0800

    charger: Fix can't show Turbo in COM when wls charging
    The charge_rate_check not check wireless charging power,
    and the ICL only 1150mA,then the charge rate is normal.
    So it can't show Turbo when using wireless charging in COM.
    
    So we need check wireless charging power to set the charge_rate
    when wireless charging online.
    
    refer to https://gerrit.mot.com/#/c/2811856/
    
    Change-Id: Ib8291b072b861fe1301c6f8f6a8e6ea174a6ace0
    Signed-off-by: Li Hongtao <liht17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3547264
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mtk_charger.c
+++ drivers/power/supply/mtk_charger.c
@@ -3682,13 +3682,34 @@ static bool mmi_has_current_tapered(struct mtk_charger *info,
 #define TURBO_CHRG_THRSH 2500
 void mmi_charge_rate_check(struct mtk_charger *info)
 {
-	int icl, rc;
+	int icl = 0;
+	int rc = 0;
 	int rp_level = 0;
 	union power_supply_propval val;
 	int qc_chg_type = 0;
 
 	if (info == NULL)
 		return;
+
+	if (!info->wl_psy) {
+		info->wl_psy = power_supply_get_by_name("wireless");
+	}
+	if (info->wl_psy) {
+		rc = power_supply_get_property(info->wl_psy,
+				POWER_SUPPLY_PROP_ONLINE, &val);
+		if (val.intval) {
+			rc = power_supply_get_property(info->wl_psy,
+				POWER_SUPPLY_PROP_POWER_NOW, &val);
+			if (val.intval >= WLS_RX_CAP_15W) {
+				info->mmi.charge_rate = POWER_SUPPLY_CHARGE_RATE_TURBO;
+			} else if(val.intval >= WLS_RX_CAP_5W) {
+				info->mmi.charge_rate = POWER_SUPPLY_CHARGE_RATE_NORMAL;
+			} else {
+				info->mmi.charge_rate = POWER_SUPPLY_CHARGE_RATE_WEAK;
+			}
+			goto end_rate_check;
+		}
+	}
 
 	icl = get_charger_input_current(info, info->chg1_dev) / 1000;
 	rc = mmi_get_prop_from_charger(info, POWER_SUPPLY_PROP_ONLINE, &val);
