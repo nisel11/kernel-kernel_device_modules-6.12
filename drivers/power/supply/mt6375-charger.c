eaebeee2ccf0ffed249a1c0f1f2d9643d81e3bff
Li Hongtao <liht17@motorola.com>
2025-09-16 13:55:03 +0800

    only wait 20s for first time
    main changes:
    For CID50 phone with usb debug enable, reboot phone, insert charger
    cable, because gadget can't ready, pmic need wait 20S to start BC12
    detection, we only wait for the first plug in.
    
    refer to https://gerrit.mot.com/#/c/3412062/
    
    Change-Id: Idcc4535346a99ca1a3526b6df7a3decf7fa0c748
    Signed-off-by: Li Hongtao <liht17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3557421
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mt6375-charger.c
+++ drivers/power/supply/mt6375-charger.c
@@ -30,6 +30,7 @@
 #include "mtk_pd_adapter.h"
 
 static bool dbg_log_en;
+static bool wait_done;
 module_param(dbg_log_en, bool, 0644);
 #define mt_dbg(dev, fmt, ...) \
 	do { \
@@ -1321,7 +1322,7 @@ static int mt6375_chg_enable_bc12(struct mt6375_chg_data *ddata, bool en)
 		/* CDP port specific process */
 		dev_info(ddata->dev, "check CDP block\n");
 		for (i = 0; i < max_wait_cnt; i++) {
-			if (is_usb_rdy(ddata->dev))
+			if (is_usb_rdy(ddata->dev) || wait_done)
 				break;
 			attach = atomic_read(&ddata->attach[0]);
 			if (attach == ATTACH_TYPE_PWR_RDY ||
@@ -1334,9 +1335,10 @@ static int mt6375_chg_enable_bc12(struct mt6375_chg_data *ddata, bool en)
 				break;
 			}
 		}
-		if (i == max_wait_cnt)
+		if (i == max_wait_cnt) {
+			wait_done = true;
 			dev_notice(ddata->dev, "%s: CDP timeout\n", __func__);
-		else
+		} else
 			dev_info(ddata->dev, "%s: CDP free\n", __func__);
 	}
 	__pm_relax(ddata->bc12_wakelock);
@@ -4436,6 +4438,7 @@ static int mt6375_chg_probe(struct platform_device *pdev)
 	char *name = NULL;
 
 	dev_info(dev, "%s: entry. 6375 charger probe now.\n", __func__);
+	wait_done = false;
 	ddata = devm_kzalloc(dev, sizeof(*ddata), GFP_KERNEL);
 	if (!ddata)
 		return -ENOMEM;
