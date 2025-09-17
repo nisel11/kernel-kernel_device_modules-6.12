e30a769b4752e005dee7203a537cd15abd52b3fa
Li Hongtao <liht17@motorola.com>
2025-09-17 15:37:22 +0800

    charge: avoid kernel panic
    As info->tcd only register in normal mode, so factory mode don't support
    OTP function. Add protection to avoid kernel panic.
    
    refer to https://gerrit.mot.com/#/c/2982370/
    
    Change-Id: Ia770b15f78dc3c01720e9afbedd48b3af92da14b
    Signed-off-by: Li Hongtao <liht17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3559698
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mtk_charger.c
+++ drivers/power/supply/mtk_charger.c
@@ -5371,8 +5371,13 @@ static void mmi_check_typec_conn_temp(struct mtk_charger *info)
 	struct thermal_zone_device *usb_conn_zone;
 	int conn_ntc = 0;
 
-	if (!info->typecotp_charger) {
+	if (!info->typecotp_charger || info->mmi.factory_mode) {
 		chr_err("%s Error: typec-otp not support\n", __func__);
+		return;
+	}
+
+	if (IS_ERR_OR_NULL(info->tcd)) {
+		chr_err("%s Error: typec cooling device not ready\n", __func__);
 		return;
 	}
 
