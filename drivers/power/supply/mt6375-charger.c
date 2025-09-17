3be20d05b75a005a0a320451fec56e5873eecee2
Li Hongtao <liht17@motorola.com>
2025-09-17 14:08:54 +0800

    charge: set usb type unknown when remove adaptor
    When enable DCP15W feature and unplug DCP only adaptor, the online
    will be hold 500ms and type is USB, so ssusb_set_mode set role to device
    and start usb when adaptor removed. So set type to UNKNOWN.
    
    refer to https://gerrit.mot.com/#/c/2945366/
    
    Change-Id: I6671620b9eda4bb7b7518faebad162a6da4c6037
    Signed-off-by: Li Hongtao <liht17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3559693
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mt6375-charger.c
+++ drivers/power/supply/mt6375-charger.c
@@ -1649,6 +1649,9 @@ static int mt6375_chg_get_property(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_TYPE:
 		mutex_lock(&ddata->attach_lock);
 		val->intval = ddata->psy_desc.type;
+		if (ddata->dcp15w.support && ddata->dcp15w.online
+			&& !atomic_read(&ddata->attach[ddata->active_idx]))
+			val->intval = POWER_SUPPLY_TYPE_UNKNOWN;
 		mutex_unlock(&ddata->attach_lock);
 		return 0;
 	case POWER_SUPPLY_PROP_CALIBRATE:
