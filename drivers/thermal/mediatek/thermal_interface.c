45dbf0d719c0f897b473ac099edaf8c1aae12d31
Shicheng Gong <gongsc1@motorola.com>
2025-09-19 19:43:28 +0800

    thermal:add virtual ntc thermal zone node
    rework from https://gerrit.mot.com/#/c/2835976/.
    1. update u_vsensor0~5 to MTK latest logic code.
    2. update thermal_zone_device_register to
    thermal_tripless_zone_device_register.
    
    Change-Id: I924eccda77a21f4563b50cb67c6aba760643343d
    Signed-off-by: Shicheng Gong <gongsc1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3564260
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/thermal/mediatek/thermal_interface.c
+++ drivers/thermal/mediatek/thermal_interface.c
@@ -42,6 +42,7 @@
 #include <leds-mtk.h>
 #endif
 #include "dvfsrc-exp.h"
+#include "thermal_core.h"
 
 #define MAX_HEADROOM		(100)
 #define CSRAM_INIT_VAL		(0x27bc86aa)
@@ -2725,6 +2726,121 @@ static void therm_intf_debugfs_init(void) {}
 static void therm_intf_debugfs_exit(void) {}
 #endif
 
+/*-----------------------------------thermal zone interface--------------------------------*/
+#define TEMP_NODE_SENSOR_NAMES "mmi,temperature-names"
+#define DEFAULT_TEMPERATURE -270000
+#define MMI_SYS_TEMP_NAME_LENGTH 20
+
+struct mmi_sys_temp_sensor {
+    struct thermal_zone_device *tz_dev;
+    const char *name;
+    int temp;
+};
+
+struct mmi_sys_temp_dev {
+    int num_sensors;
+    struct mmi_sys_temp_sensor *sensor;
+};
+
+static int mmi_sys_temp_get(struct thermal_zone_device *thermal,
+                int *temp)
+{
+    struct mmi_sys_temp_sensor *sensor = thermal->devdata;
+    int i;
+    int len = MMI_SYS_TEMP_NAME_LENGTH;
+
+    if (!sensor)
+        return -EINVAL;
+
+    sensor->temp = DEFAULT_TEMPERATURE;
+
+    /* Search for matching user virtual sensor */
+    for (i = 0; i < USER_VSENSOR_NUM; i++) {
+        if (!strncasecmp(sensor->name, u_vsensor[i].user_vsensor_name, len)) {
+            sensor->temp = u_vsensor[i].temp;
+            break;
+        }
+    }
+
+    *temp = sensor->temp;
+    pr_info("%s=%d\n", sensor->name, *temp);
+
+    return 0;
+}
+
+static struct thermal_zone_device_ops mmi_sys_temp_ops = {
+    .get_temp = mmi_sys_temp_get,
+};
+
+static int mmi_thermal_zone_register(struct platform_device *pdev)
+{
+    struct mmi_sys_temp_dev *sys_temp_dev;
+    int num_sensors;
+    int num_registered = 0;
+    int i, ret;
+
+    num_sensors = of_property_count_strings(pdev->dev.of_node, TEMP_NODE_SENSOR_NAMES);
+    dev_info(&pdev->dev, "num_sensors = %d\n", num_sensors);
+    if (num_sensors <= 0) {
+        dev_err(&pdev->dev,
+            "bad number of sensors: %d\n", num_sensors);
+        return 0;
+    }
+
+    sys_temp_dev = devm_kzalloc(&pdev->dev, sizeof(struct mmi_sys_temp_dev),
+                    GFP_KERNEL);
+    if (!sys_temp_dev) {
+        dev_err(&pdev->dev,
+            "Unable to alloc memory for sys_temp_dev\n");
+        return -ENOMEM;
+    }
+    sys_temp_dev->num_sensors = num_sensors;
+
+    sys_temp_dev->sensor = devm_kcalloc(&pdev->dev, num_sensors,
+                    sizeof(struct mmi_sys_temp_sensor), GFP_KERNEL);
+    if (!sys_temp_dev->sensor) {
+        dev_err(&pdev->dev,
+            "Unable to alloc memory for sensor\n");
+        return -ENOMEM;
+    }
+
+    for (i = 0; i < num_sensors; i++) {
+        ret = of_property_read_string_index(pdev->dev.of_node,
+                        TEMP_NODE_SENSOR_NAMES, i,
+                        &sys_temp_dev->sensor[i].name);
+        if (ret) {
+            dev_err(&pdev->dev, "Unable to read of_prop string\n");
+            goto err_thermal_unreg;
+        }
+
+        sys_temp_dev->sensor[i].temp = DEFAULT_TEMPERATURE;
+
+        sys_temp_dev->sensor[i].tz_dev =
+           thermal_tripless_zone_device_register(sys_temp_dev->sensor[i].name,
+                        &sys_temp_dev->sensor[i],
+                        &mmi_sys_temp_ops,
+                        NULL);
+        if (IS_ERR(sys_temp_dev->sensor[i].tz_dev)) {
+            dev_err(&pdev->dev,
+                "thermal_zone_device_register() failed.\n");
+            ret = PTR_ERR(sys_temp_dev->sensor[i].tz_dev);
+            goto err_thermal_unreg;
+        }
+        num_registered = i + 1;
+    }
+
+    return 0;
+
+err_thermal_unreg:
+    for (i = 0; i < num_registered; i++) {
+        if (sys_temp_dev->sensor[i].tz_dev && !IS_ERR(sys_temp_dev->sensor[i].tz_dev)) {
+            thermal_zone_device_unregister(sys_temp_dev->sensor[i].tz_dev);
+        }
+    }
+
+    return ret;
+}
+/*-----------------------------------thermal zone interface end--------------------------------*/
 
 static const struct of_device_id therm_intf_of_match[] = {
 	{ .compatible = "mediatek,therm_intf", },
@@ -3155,6 +3271,9 @@ static int therm_intf_probe(struct platform_device *pdev)
 		mtk_leds_register_notifier(&leds_init_notifier);
 #endif
 
+	//prepare for thermal_zone
+	mmi_thermal_zone_register(pdev);
+
 	therm_np = pdev->dev.of_node;
 	of_property_read_s32(therm_np, "throttle-at-boot", &tm_data.boot.throttle_at_boot);
 	of_property_read_string(therm_np, "boot-sensor", &(tm_data.boot.sensor));
@@ -3198,6 +3317,7 @@ static int therm_intf_probe(struct platform_device *pdev)
 		pr_debug("%s : Failed to register PM notifier.\n", __func__);
 		return ret;
 	}
+
 	return 0;
 }
 
