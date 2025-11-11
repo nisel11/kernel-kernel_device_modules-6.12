0ef24f25e649369c4a8cb8bf42543e804181e401
HuAnDong <tinno80@motorola.com>
2025-11-11 14:52:38 +0800

    bogota: drv: imx882 eeprom bring up [1/2]
    details:
        imx882 eeprom bring up
    
    Change-Id: I54be2a5639b53bc5938106e22afdb0fa120956bc
    Signed-off-by: HuAnDong <tinno80@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3632146
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Mingfeng Yang <yangmf3@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
+++ drivers/misc/mediatek/cam_cal/src/custom/cam_cal_list.c
@@ -21,6 +21,7 @@ extern unsigned int mot_bogota_sc820_read_region(struct i2c_client *client, unsi
 
 struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 #if defined(CONFIG_MOT_BOGOTA_CAMERA_PROJECT)
+	{MOT_BOGOTA_IMX882_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{MOT_BOGOTA_GC08A8_SENSOR_ID, 0x62, mot_bogota_gc08a8_read_region},  //otp
 	{MOT_BOGOTA_SC820_SENSOR_ID, 0x6C, mot_bogota_sc820_read_region},
 #else
