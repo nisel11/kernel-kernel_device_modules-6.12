662bf7d41d77b011390f5c63229b1138e461c2f4
ot_tushar.gupta <ot_tushar.gupta@mediatek.com>
2025-11-13 23:25:36 +0800

    [ALPS10278756] mmdvfs: add ap ccf enable mutex func
    [Description]
    add ccf enable mutex func for mmdvfs
    
    [Test]
    boot pass
    
    MTK-Commit-Id: 125a49e41dda0544f87b01a0684decf7132fa602
    
    Change-Id: Ia2780f95f63bba419ef5409c5bb6ec696cc01682
    Signed-off-by: ot_tushar.gupta <ot_tushar.gupta@mediatek.com>
    CR-Id: ALPS10278756
    Feature: mmdvfs

--- include/linux/soc/mediatek/mtk_mmdvfs.h
+++ include/linux/soc/mediatek/mtk_mmdvfs.h
@@ -8,6 +8,7 @@
 #include <linux/kernel.h>
 
 typedef void (*record_opp)(const u8 opp);
+typedef void (*ap_ccf)(const bool enable);
 
 #if IS_ENABLED(CONFIG_MTK_MMDVFS)
 
@@ -40,16 +41,23 @@ int mmdvfs_tracing_mark_write(char *fmt, ...);
 
 int register_mmdvfs_notifier(struct notifier_block *nb);
 int unregister_mmdvfs_notifier(struct notifier_block *nb);
+int mmdvfs_set_ccf_enable_mutex(bool lock);
+int mmdvfs_set_vcp_cb_ready(bool enable);
 int mmdvfs_ap_ccf_enable(bool enable);
 int mmdvfs_set_force_step(int force_step);
 int mmdvfs_set_vote_step(int vote_step);
 void mmdvfs_debug_record_opp_set_fp(record_opp fp);
+void mmdvfs_ap_ccf_enable_notifier_set_fp(ap_ccf fp);
 void mtk_mmdvfs_aov_enable(bool enable);
 #else
 static inline int register_mmdvfs_notifier(struct notifier_block *nb)
 { return -EINVAL; }
 static inline int unregister_mmdvfs_notifier(struct notifier_block *nb)
 { return -EINVAL; }
+static inline int mmdvfs_set_ccf_enable_mutex(bool lock)
+{ return 0; }
+static inline int mmdvfs_set_vcp_cb_ready(bool enable)
+{ return 0; }
 static inline int mmdvfs_ap_ccf_enable(bool enable)
 { return 0; }
 static inline int mmdvfs_set_force_step(int new_force_step)
@@ -57,6 +65,7 @@ static inline int mmdvfs_set_force_step(int new_force_step)
 static inline int mmdvfs_set_vote_step(int new_force_step)
 { return 0; }
 static inline void mmdvfs_debug_record_opp_set_fp(record_opp fp) {}
+static inline void mmdvfs_ap_ccf_enable_notifier_set_fp(ap_ccf fp) {}
 static inline void mtk_mmdvfs_aov_enable(bool enable) {}
 #endif /* CONFIG_MTK_MMDVFS */
 
