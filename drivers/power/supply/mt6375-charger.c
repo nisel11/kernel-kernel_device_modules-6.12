215fd2fcba62194618ca92e61d75669299795678
Li Hongtao <liht17@motorola.com>
2026-01-21 11:48:22 +0800

    charging: rerun BC12 other than SDP and CDP
    Rerun bc1.2 has an impact on PD certification,
    so it is removed in (CR).
    But too much content was removed,
    only SDP and CDP need to be removed,
    So add content other than SDP and CDP.
    
    refer from: https://gerrit.mot.com/#/c/3709406/
    
    Change-Id: I9178a53ccd1c5a36263220c544107ea6b02c4c19
    Signed-off-by: Li Hongtao <liht17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3729470
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mt6375-charger.c
+++ drivers/power/supply/mt6375-charger.c
@@ -317,6 +317,8 @@ struct mt6375_chg_data {
 
 	struct dcp15w dcp15w;
 
+	bool mmi_bc12_rerun_done;
+
 	struct wakeup_source *bc12_wakelock;
 };
 
@@ -1194,6 +1196,8 @@ static void mt6375_chg_pwr_rdy_process(struct mt6375_chg_data *ddata)
 	ddata->pwr_rdy = val;
 	mt_dbg(ddata->dev, "pwr_rdy=%d\n", val);
 
+	ddata->mmi_bc12_rerun_done = false;
+
 	if (ddata->qc_dev) {
 		adapter_dev_reset_chg_type(ddata->qc_dev);
 		ddata->pulse_cnt = 0;
@@ -1427,6 +1431,18 @@ static void mt6375_chg_bc12_work_func(struct work_struct *work)
 		break;
 	}
 
+	if ((attach < ATTACH_TYPE_PD) &&
+		(val == PORT_STAT_UNKNOWN_TA)
+		&& (!ddata->mmi_bc12_rerun_done)) {
+		bc12_en = true;
+		rpt_psy = false;
+		ddata->mmi_bc12_rerun_done = true;
+		mt6375_chg_enable_bc12(ddata, false); //need to toggle bc12 en for rerun bc12
+		dev_info(ddata->dev, "rerun bc12 for port stat = %s\n", mt6375_port_stat_names[val]);
+		msleep(500);
+		goto out;
+	}
+
 	switch (val) {
 	case PORT_STAT_NOINFO:
 		bc12_ctrl = false;
@@ -1487,6 +1503,7 @@ static void mt6375_chg_bc12_work_func(struct work_struct *work)
 		dev_info_ratelimited(ddata->dev, "%s, Unknown port stat(%d)\n", __func__, val);
 		goto out;
 	}
+	dev_info(ddata->dev, "port stat = %s\n", mt6375_port_stat_names[val]);
 out:
 	mutex_unlock(&ddata->attach_lock);
 	if (bc12_ctrl) {
