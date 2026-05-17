422f768314743586c751193cae4c05bdacd9b204
Nisel <nisel11good@gmail.com>
2026-05-17 18:15:34 +0000

    mm_monitor: Add MT6781 support
    just copy from mt6789

--- drivers/misc/mediatek/mm_monitor/mtk-mm-monitor-controller.c
+++ drivers/misc/mediatek/mm_monitor/mtk-mm-monitor-controller.c
@@ -14,6 +14,55 @@
 #include <soc/mediatek/smi.h>
 #include <linux/iommu.h>
 #include "mtk-smmu-v3.h"
+#include <dt-bindings/memory/mt6789-smi-pd.h>
+
+u16 get_freq_from_mux_id(enum MUX_ID id)
+{
+	return 0;
+}
+EXPORT_SYMBOL(get_freq_from_mux_id);
+
+u32 get_mmmc_subsys_max(void)
+{
+	return 28;
+}
+EXPORT_SYMBOL(get_mmmc_subsys_max);
+
+u32 get_mminfra_pd(void)
+{
+	return MT6991_SMI_PD_MMINFRA1;
+}
+EXPORT_SYMBOL(get_mminfra_pd);
+
+s32 is_valid_offset_value(u32 hw, u32 id, u32 offset, u32 value)
+{
+	return 0;
+}
+EXPORT_SYMBOL(is_valid_offset_value);
+
+void enable_mminfra_funnel(void)
+{
+	return;
+}
+EXPORT_SYMBOL(enable_mminfra_funnel);
+
+void mminfra_fake_engine_bus_settings(void)
+{
+	return;
+}
+EXPORT_SYMBOL(mminfra_fake_engine_bus_settings);
+
+void emi_moniter_settings(void)
+{
+	return;
+}
+EXPORT_SYMBOL(emi_moniter_settings);
+
+u32 get_power_domains(int index)
+{
+	return 0;
+}
+EXPORT_SYMBOL(get_power_domains);
 
 #define CREATE_TRACE_POINTS
 #include "mmmc_events.h"
--- kernel/kleaf/mgk_64.bzl
+++ kernel/kleaf/mgk_64.bzl
@@ -1423,6 +1423,7 @@ _mgk_64_kleaf_platform_modules = {
     "//kernel_device_modules-{}/drivers/misc/mediatek/mdp:mdp_drv_mt6895".format(kernel_version): "mt6895",
     "//kernel_device_modules-{}/drivers/misc/mediatek/mdp:mdp_drv_mt6991".format(kernel_version): "mt6991",
     "//kernel_device_modules-{}/drivers/misc/mediatek/memory-amms:memory-amms".format(kernel_version): "mt6781",
+    "//kernel_device_modules-{}/drivers/misc/mediatek/mm_monitor:mm-monitor-platform-mt6781".format(kernel_version): "mt6781",
     "//kernel_device_modules-{}/drivers/misc/mediatek/mm_monitor:mm-monitor-platform-mt6789".format(kernel_version): "mt6789",
     "//kernel_device_modules-{}/drivers/misc/mediatek/mm_monitor:mm-monitor-platform-mt6855".format(kernel_version): "mt6855",
     "//kernel_device_modules-{}/drivers/misc/mediatek/mm_monitor:mm-monitor-platform-mt6895".format(kernel_version): "mt6895",
